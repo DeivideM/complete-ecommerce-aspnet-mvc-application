ae6398360f9ab20bc47999ea771587725cb1eb68
DeivideM <deivide_martins@hotmail.com>
2025-10-31 09:58:39 -0300

    Added CRUD for Actors and NotFound page

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -6,14 +6,19 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        //public Guid PublicId { get; set; } = Guid.NewGuid();
 
         [Display(Name = "Full Name")]
+        [StringLength(500, MinimumLength = 2, ErrorMessage = "Full name must be between 2 and 500 characters")]
+        [Required(ErrorMessage ="Full name is required")]
         public string FullName { get; set; } = string.Empty;
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Bio is required")]
         public string Bio { get; set; } = string.Empty;
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile picture is required")]
         public string ProfilePictureURL { get; set; } = string.Empty;
 
         //Relationships
